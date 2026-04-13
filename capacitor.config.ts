bea3f449abac5897315c59a4f1db67ada51c4e8a
zwartebliksem1 <83717899+zwartebliksem1@users.noreply.github.com>
2026-04-13 20:10:15 +0200

    Feat: ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.luukloef.soundsnap',
+  appName: 'soundsnap',
+  webDir: 'dist'
+};
+
+export default config;
